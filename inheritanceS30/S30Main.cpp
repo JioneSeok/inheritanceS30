21944fdf212015f651c135c9c1014f5f9ad0777a
Jiwon Seok <jwseok03@gmail.com>
2023-06-01 20:22:13 +0900

    초기 커밋.

---
+++ inheritanceS30/S30Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class mm {
+public:
+	mm() { a = 4 + 300; cout << "mm\n"; }
+	mm(int b) { a = b + 100; cout << "mmb\n"; }
+	int a;
+};
+class nn : public mm {
+public:
+	nn() { a = 11;  cout << "nn\n"; }
+	nn(int b) { c = a;  a = b; mm::mm(a);  cout << "nnb\n"; }
+	int c;
+};
+class oo : public nn {
+public:
+	oo() { a = 22; cout << "oo\n"; }
+	oo(int b) { d = a; a = b; nn::nn(a); cout << "oob\n"; }
+	int d;
+};
+int main() {
+	oo pp(5);
+	cout << pp.a << endl;
+	cout << pp.c << endl;
+	return 123;
+}
